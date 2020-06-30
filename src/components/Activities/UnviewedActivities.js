7c85dcaa061d6723430dd736db4e218bc729d183
Stephen Collins <sgcollins@outlook.com>
2020-06-30 14:04:37 -0400

    updated pagination

--- src/components/Activities/UnviewedActivities.js
+++ src/components/Activities/UnviewedActivities.js
@@ -1,12 +1,20 @@
-import PropTypes from "prop-types";
-import React, { useState } from "react";
-import { graphql } from "babel-plugin-relay/macro";
-import { Grid, Button } from "@material-ui/core";
-import { useRefetch } from "relay-hooks";
+import PropTypes from 'prop-types';
+import React, { useState, useEffect } from 'react';
+import { graphql } from 'babel-plugin-relay/macro';
+import ActivityCard from './ActivityCard';
+import { Grid, Button } from '@material-ui/core';
+import { usePagination } from 'relay-hooks';
 
 const propTypes = {
   authInfo: PropTypes.object.isRequired,
 };
+
+const getCurrentUrl = () => {
+  const { protocol, host } = window.location;
+  return `${protocol}//${host}`;
+};
+const redirectUri = `${getCurrentUrl()}/login`;
+const source = 'google';
 
 const fragmentSpec = graphql`
   fragment UnviewedActivities_authInfo on AuthInfo
@@ -37,17 +45,47 @@ const fragmentSpec = graphql`
   }
 `;
 
-const fetchQuery = graphql`
-  query UnviewedActivitiesRefetchQuery($count: Int!, $cursor: ID) {
-    authInfo {
-      ...UnviewedActivities_authInfo @arguments(cursor: $cursor, count: $count)
+const connectionConfig = {
+  direction: 'forward',
+  query: graphql`
+    query UnviewedActivitiesRefetchQuery(
+      $redirectUri: String!
+      $source: String!
+      $count: Int!
+      $cursor: ID
+    ) {
+      authInfo(redirectUri: $redirectUri, source: $source) {
+        ...UnviewedActivities_authInfo
+          @arguments(cursor: $cursor, count: $count)
+      }
     }
-  }
-`;
+  `,
+  getConnectionFromProps(props) {
+    return props.viewer.list;
+  },
+  getFragmentVariables(previousVariables, totalCount) {
+    return {
+      ...previousVariables,
+      count: totalCount,
+    };
+  },
+  getVariables(_props, { count, cursor }) {
+    return {
+      count: count,
+      cursor,
+      redirectUri,
+      source,
+    };
+  },
+};
 
 const UnviewedActivities = (props) => {
-  const [authInfo, refetch] = useRefetch(fragmentSpec, props.authInfo);
+  const [authInfo, { hasMore, isLoading, loadMore }] = usePagination(
+    fragmentSpec,
+    props.authInfo
+  );
   const [, setLoads] = useState(0);
+  const [selected, setSelected] = useState([]);
 
   const {
     viewer: {
@@ -55,20 +93,54 @@ const UnviewedActivities = (props) => {
     },
   } = authInfo;
 
+  useEffect(() => {
+    const key = 'app:selectedActivities';
+    const item = localStorage.getItem(key);
+    if (item) {
+      setSelected(JSON.parse(item));
+    } else {
+      localStorage.setItem(key, JSON.stringify([]));
+    }
+  }, []);
+
   const loadNextPage = () => {
-    refetch(
-      fetchQuery,
-      { count: 10 },
+    if (!hasMore() || isLoading()) {
+      return;
+    }
+    loadMore(
+      connectionConfig,
+      5,
       null,
       () => {
         setLoads((v) => v + 1);
-        console.log("loaded more");
+        console.log('loaded more');
       },
       {
         force: true,
       }
     );
   };
+
+  const addActivity = (id, pass) => {
+    const key = 'app:selectedActivities';
+    const stored = localStorage.getItem(key) || [];
+    const parsed = stored instanceof Array ? stored : JSON.parse(stored);
+    const selected = [...parsed, { id, pass }];
+    localStorage.setItem(key, JSON.stringify(selected));
+    setSelected(selected);
+
+    const remaining = edges.filter(
+      ({ node: a }) => !selected.some((s) => s.id === a.activityId)
+    );
+
+    if (remaining.length === 1) {
+      loadNextPage();
+    }
+  };
+
+  const remaining = edges.filter(
+    ({ node: a }) => !selected.some((s) => s.id === a.activityId)
+  );
 
   return (
     <>
@@ -80,9 +152,9 @@ const UnviewedActivities = (props) => {
         alignItems="center"
         spacing={2}
       >
-        {edges.map(({ node: activity }) => (
+        {remaining.map(({ node: activity }) => (
           <Grid item key={activity.activityId}>
-            <div>{activity.name}</div>
+            <ActivityCard {...activity} addActivity={addActivity} />
           </Grid>
         ))}
       </Grid>
